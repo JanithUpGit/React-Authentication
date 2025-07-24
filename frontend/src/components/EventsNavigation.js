3ed1263c4ab33719f03db18a4313d42d3261cbd5
P.A.J.U.Pallewela <janithup10@gmail.com>
2025-07-24 23:44:25 +0530

    meke token expire

--- frontend/src/components/EventsNavigation.js
+++ frontend/src/components/EventsNavigation.js
@@ -5,7 +5,7 @@ import classes from './EventsNavigation.module.css';
 function EventsNavigation() {
   const token = useRouteLoaderData('root');
   console.log(token);
-)
+  
   return (
     <header className={classes.header}>
       <nav>
